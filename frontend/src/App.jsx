900b1d1deedf487c47cd535b1bfc9e3d19705331
Shushri <shubhiarnm@gmail.com>
2025-10-05 00:19:52 +0530

    added components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import Navbar from './components/Navbar/navbar'
+import home from './pages/home'
+import cart from './pages/cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/navbar'
+import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<home/>}/>
+        <Route path='/cart' element={<cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import header from '../components/header'
+const home = () => {
+  return (
+    <div>
+      <header/>
+    </div>
+  )
+}
+
+export default home
